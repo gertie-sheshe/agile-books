2f327449ce93d75cc81c0dd81c6a05818d821239
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-08-19 20:48:06 +0300

    add description text + buttons

---
+++ src/components/landingpage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './landingpage.scss';
+
+const LandingPage = () => (
+    <div className="auth-card">
+        <div>
+            <h1>Agile Books</h1>
+            <p>Read your books productively</p>
+        </div>
+        <div>
+            <button>Log in/Sign up</button>
+            <button>Demo</button>
+        </div>
+    </div>
+);
+
+export default LandingPage;
